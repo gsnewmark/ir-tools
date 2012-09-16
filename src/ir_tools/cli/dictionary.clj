617c9ff8b30877404591b0e2fdb9843c35b53c8c
gsnewmark <gsnewmark@meta.ua>
2012-09-16 16:42:21 +0300

    Update project file.

--- src/ir_tools/cli/dictionary.clj
+++ src/ir_tools/cli/dictionary.clj
@@ -1,6 +1,6 @@
 (ns ir-tools.cli.dictionary
   "CLI for dictionary API."
-  (:require [ir-tools.api.dictionary :as dict-api]
+  (:require [ir-tools.api.dictionary :as api]
             [ir-tools.cli.common :as common]))
 
 
@@ -24,21 +24,21 @@ the arguments."
                ;; of words in those files as well as their sizes in bytes.
                sizes (doall
                       (map
-                       (partial dict-api/fill-dict-from-file
-                                dict-api/dictionary)
+                       (partial api/fill-dict-from-file
+                                api/dictionary)
                        sources))
                total-words (apply + (map :tokens-count sizes))
                total-size (apply + (map :size sizes))]
            ;; Print some info about files.
            (dorun (map common/print-file-info sources sizes))
            ;; Write a dictionary to a file.
-           (dict-api/write-dict-to-file dict-api/dictionary file-to-write)
+           (api/write-dict-to-file api/dictionary file-to-write)
            ;; Print current dictionary size, total words count and total size.
            (println (apply str (repeat 80 "-")))
            (println)
            (println "Total amount of words in all files:" total-words)
            (println "Total size of all files (in bytes):" total-size)
            (println "Amount of words in a dictionary:"
-                    (dict-api/dict-size dict-api/dictionary)))))
+                    (api/dict-size api/dictionary)))))
       (println (str "You must provide at least 2 arguments - where to "
                     "write a dictionary and source of words.")))))
