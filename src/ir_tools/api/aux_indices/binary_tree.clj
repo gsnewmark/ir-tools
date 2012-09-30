44bf13d29432726948c90a4dbc7ee2a29c92426b
gsnewmark <gsnewmark@meta.ua>
2012-09-30 18:09:30 +0300

    Add simple implementation of a binary tree insert.

--- src/ir_tools/api/aux_indices/binary_tree.clj
+++ src/ir_tools/api/aux_indices/binary_tree.clj
@@ -12,12 +12,23 @@
 
 ;; Describes a basic binary tree node with a value, left and right sons as
 ;; well as a link to parent of a given node.
-(defrecord TreeNode [key left right parent])
+(defrecord TreeNode [key left right])
 
 (defn create-tree
   "Constructor to create a binary tree with a given element as an initial
 root."
   [root-element]
-  (->TreeNode root-element nil nil nil))
+  (->TreeNode root-element nil nil))
+
+(defn add-node
+  "Adds a node at a proper position of a given binary tree."
+  [node tree]
+  (cond
+   (nil? tree) (create-tree node)
+   (< (.compareTo node (:key tree)) 0)
+   (->TreeNode (:key tree) (add-node node (:left tree)) (:right tree))
+   (> (.compareTo node (:key tree)) 0)
+   (->TreeNode (:key tree) (:left tree) (add-node node (:right tree)))
+   :else tree))
 
 ;; ## Private API
