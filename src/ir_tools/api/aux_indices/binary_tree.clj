ae626aa06c3e5c16e3eba6985457957a50e3c4e1
gsnewmark <gsnewmark@meta.ua>
2012-09-30 21:19:34 +0300

    Add binary tree generation from inverted index.

--- src/ir_tools/api/aux_indices/binary_tree.clj
+++ src/ir_tools/api/aux_indices/binary_tree.clj
@@ -6,30 +6,50 @@
 
 ;; ## Forward declarations
 
-(declare)
+(declare create-tree add-node add-term-to-tree)
 
-;; ## Public API
+;; ## Data Structures
 
 ;; Describes a basic binary tree node with a value, left and right sons as
 ;; well as a link to parent of a given node.
 (defrecord TreeNode [key left right])
 
-(defn create-tree
+;; Binary trees with terms and reverted terms.
+(def terms-tree (atom nil))
+(def inverted-terms-tree (atom nil))
+
+;; ## Public API
+
+(defn inverted-index->tree
+  "Add all words from a given inverted index to a given binary tree and
+binary tree with inverted terms."
+  [i-ref t-ref it-ref]
+  (let [terms (keys @i-ref)]
+    (doseq [t terms]
+      (add-term-to-tree t-ref it-ref t))))
+
+(defn add-term-to-tree
+  "Adds a term to a binary tree and inverted binary tree."
+  [tree-ref itree-ref term]
+  (swap! tree-ref add-node term)
+  (swap! itree-ref add-node (cstr/reverse term)))
+
+;; ## Private API
+
+(defn- create-tree
   "Constructor to create a binary tree with a given element as an initial
 root."
   [root-element]
   (->TreeNode root-element nil nil))
 
-(defn add-node
+(defn- add-node
   "Adds a node at a proper position of a given binary tree."
-  [node tree]
+  [tree node]
   (cond
    (nil? tree)
     (create-tree node)
    (< (.compareTo node (:key tree)) 0)
-    (->TreeNode (:key tree) (add-node node (:left tree)) (:right tree))
+    (->TreeNode (:key tree) (add-node (:left tree) node) (:right tree))
    (> (.compareTo node (:key tree)) 0)
-    (->TreeNode (:key tree) (:left tree) (add-node node (:right tree)))
+    (->TreeNode (:key tree) (:left tree) (add-node (:right tree) node))
    :else tree))
-
-;; ## Private API
