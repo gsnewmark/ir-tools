23a0257ac6fb65bceabc64da7f59878424edc547
gsnewmark <gsnewmark@meta.ua>
2012-10-09 22:35:08 +0300

    Add ugly files join procedure.

--- src/ir_tools/api/spimi_index_builder.clj
+++ src/ir_tools/api/spimi_index_builder.clj
@@ -2,7 +2,6 @@
   "Generates an (inverted) index for a collection of documents using
 the SPIMI algorithm (http://nlp.stanford.edu/IR-book/html/htmledition/single-pass-in-memory-indexing-1.html)."
   (:require [clojure.java.io :as io]
-            [clojure.string :as cstr]
             [ir-tools.api.inverted-index :as index-api]
             [ir-tools.api.common :as common-api])
   (:import [java.io File]))
@@ -10,7 +9,7 @@ the SPIMI algorithm (http://nlp.stanford.edu/IR-book/html/htmledition/single-pas
 ;; ## Forward Declarations
 
 (declare get-file-names get-available-memory has-memory?
-         write-temp-index-to-file join-files)
+         write-temp-index-to-file join-files!)
 
 ;; ## Public API
 
@@ -20,8 +19,8 @@ the SPIMI algorithm (http://nlp.stanford.edu/IR-book/html/htmledition/single-pas
 
 (defn process-collection
   "Adds all terms from a given collection (name of a directory with files)
-to one inverted index."
-  [col]
+to one inverted index stored in a file with name res."
+  [col res]
   (let [filenames (get-file-names col)
         i (atom 1)]
     (common-api/fill-doc-ids index-api/doc-ids filenames)
@@ -31,21 +30,48 @@ to one inverted index."
       (index-api/fill-index-from-file index-api/index
                                       index-api/doc-ids file))
     (write-temp-index-to-file i)
-    (join-files @i)
+    (join-files! @i res)
     nil))
 
 ;; ## Private API
 
-(defn- join-files
-  "Join all temporary index files (with names 1, 2, 3...) into one big index."
-  [next-num]
+(defn- join-files!
+  "Join all temporary index files (with names 1, 2, 3...) into one big index
+stored in a file with a name result."
+  [next-num result]
   (let [temp-names (map str (range 1 next-num))
-        readers (map io/reader temp-names)]
+        readers (map io/reader temp-names)
+        res (io/writer result)]
     (try
-      (loop [lines (map #(.readLine %) readers)]
-        (when-not (every? nil? lines)
-          (recur (map #(.readLine %) readers))))
-      (finally (doseq [r readers] (. r close))))))
+      (loop [lines (map #(vector % (.readLine %)) readers)]
+        (let [;; Remove empty readers
+              non-nil (remove (comp nil? second) lines)
+              ;; Transform read strings to data structures
+              evaled (map #(vector (first %) (read-string (second %)))
+                          non-nil)]
+          (when-not (empty? evaled)
+            (let [;; Find 'minimal' word among current lines
+                  min-word (first (sort (map (comp first second) evaled)))
+                  ;; Find set with docIDs for a current 'minimal' word
+                  to-write-val (reduce
+                                into
+                                ;; Extract sets with docIDs
+                                (map (comp second second)
+                                     ;; Find elements with 'minimal' word
+                                     (filter #(= min-word (first (second %)))
+                                             evaled)))
+                  ;; Fully formed vector to write into file
+                  to-write [min-word to-write-val]
+                  recur-func (fn [[reader [word pos]]]
+                               (if (= word min-word)
+                                 [reader (.readLine reader)]
+                                 [reader (str [word pos])]))]
+              (.write res (str to-write))
+              (.newLine res)
+              (recur (map recur-func evaled))))))
+      (finally
+        (.flush res)
+        (doseq [r (conj readers res)] (.close r))))))
 
 (defn- write-temp-index-to-file
   [i]
@@ -80,7 +106,7 @@ what percent of a memory should remain free."
      (let [r (Runtime/getRuntime)
            total-mem (. r maxMemory)
            available-mem (get-available-memory)]
-       (println available-mem)
+       ;(println available-mem)
        (and (or (> available-mem (* 2 memory-to-use))
                 (> memory-to-use total-mem))
             (>= (/ available-mem total-mem) free-percent)))))
