b55d0acf7c4b086fe42658eaf69950957bea05a4
gsnewmark <gsnewmark@meta.ua>
2012-10-27 00:13:15 +0300

    Working fb2 parser.

--- src/ir_tools/api/fb2_tools.clj
+++ src/ir_tools/api/fb2_tools.clj
@@ -1,33 +1,43 @@
 (ns ir-tools.api.fb2-tools
   "Functions to correctly parse FB2 files (including zoning)."
-  (:require [clojure.xml :as cxml]))
+  (:require [clojure [xml :as cxml]
+                     [string :as cstr]]
+            [ir-tools.api [common :as common]
+                          [positional-index :as p-index]]))
 
 
 ;; ## Forward declarations
 
-(declare process-one-level)
+(declare process-one-level get-text-attr process-one-level extract-text)
+
+;; ## Data Structures
+
+(def positional-index-title (atom (sorted-map)))
+(def positional-index-author (atom (sorted-map)))
+(def positional-index- (atom (sorted-map)))
+(def doc-ids (atom {}))
 
 ;; ## Public API
 
-;; ## Private API
-
-(defn- extract-info
+(defn extract-info
   "Extracts required title info (author, title) and actual body
 text."
   [fb2-xml]
   (let [fb2-map (cxml/parse fb2-xml)
-        name (first
+        name (extract-text
               (get-text-attr
                fb2-map [:description :title-info :author :first-name]))
-        surname (first
+        surname (extract-text
                  (get-text-attr
                   fb2-map [:description :title-info :author :last-name]))
         author (str name " " surname)
-        title (first
+        title (extract-text
                (get-text-attr fb2-map [:description :title-info :book-title]))
         ;; TODO grab only text from body
-        body (get-text-attr fb2-map [:body])]
+        body (extract-text (get-text-attr fb2-map [:body]))]
     {:author author :title title :body body}))
+
+;; ## Private API
 
 (defn- get-text-attr
   "Gets a given part of a map produced by cxml/parse (elt) specified by a list
@@ -44,3 +54,31 @@ gets values of all element with specified tag."
   (if-not (= tag :content)
     (for [e elt :when (= tag (:tag e))] e)
     (apply concat (map tag elt))))
+
+(defn- extract-text
+  "Retrieves actual text from a section of a parsed xml."
+  [parsed-body]
+  (cstr/join
+   "\n"
+   (flatten
+    (remove nil?
+            (doall
+             (for [el parsed-body]
+               (if (string? el)
+                 el
+                 (when-not (= :title (:tag el))
+                   (doall (for [s (:content el)]
+                            (let [c (:content s)]
+                              (extract-text c))))))))))))
+
+
+
+
+
+
+
+
+
+
+
+
